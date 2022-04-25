c1eea7938b2b78b70c27ccd6df97d6fc375c2650
Cassio Placido <silenozlt@hotmail.com>
2022-04-25 16:34:30 -0300

    step 1

--- beerlog/models.py
+++ beerlog/models.py
@@ -3,6 +3,7 @@ from sqlmodel import select
 from datetime import datetime
 from typing import Optional
 from pydantic import validator
+from statistics import mean
 
 
 class Beer(SQLModel, table=True):
@@ -13,6 +14,7 @@ class Beer(SQLModel, table=True):
     image: int
     cost: int
     rate: int = 0
+    date : datetime =Field(default_factory=datetime.now)
 
 
     @validator("flavor", "image", "cost")
@@ -20,6 +22,18 @@ class Beer(SQLModel, table=True):
         if v < 1 or v > 10:
             raise RuntimeError(f"{field.name} must be between 1 and 10")
         return v
+
+    @validator("rate", always=True)
+    def  calculate_rate(cls, v, values):
+        rate = mean(
+            [
+                values["flavor"], 
+                values["image"], 
+                values["cost"]
+            ]
+        )
+        return int(rate)
+
 
 try:
     brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=8, cost=10)
